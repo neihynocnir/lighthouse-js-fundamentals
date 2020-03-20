77a3d75cbbace683bf96591797fb5d7f816bdee9
Neihy Rincon <neihy.rincon@gmail.com>
2020-03-20 23:51:45 +0000

    how many hundreds exercise

---
+++ progIntro/howMany.js
@@ -0,0 +1,12 @@
+function howManyHundreds(num){
+  
+  let reminder = num % 100;
+  let value = (num - reminder) / 100
+  return value
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
